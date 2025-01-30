ab66a0776c56f229b436033f3d0ca63a536a5a4c
Hemant Agrawal <ha.hemantagrawal@gmail.com>
2025-01-30 05:58:29 +0000

    Fix Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,41 +27,21 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+    const formData = new FormData(e.target);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "JavaScript Mastery",
-          from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+    const response = await fetch("https://formspree.io/f/xovjwazd", {
+      method: "POST",
+      body: formData,
+      headers: { "Accept": "application/json" },
+    });
+    setForm({
+    name: "",
+    email: "",
+    message: "",
+  })
   };
 
   return (
